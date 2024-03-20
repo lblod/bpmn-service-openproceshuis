d6dc9b0b9d640fee09850d5b3ee47ba48f555e36
Martijn Bogaert <martijn.bogaert@sirus.be>
2024-03-20 09:52:24 +0100

    Retrieve group uri on post request

--- app.js
+++ app.js
@@ -8,19 +8,35 @@ import { existsSync } from "fs";
 import {
   generateUpdateQuery,
   generateUploadResourceUriSelectQuery,
+  generateGroupUriSelectQuery,
 } from "./sparql-queries.js";
 
-const storageFolderPath = "/share/";
+const STORAGE_FOLDER_PATH = "/share/";
+const HEADER_MU_SESSION_ID = "mu-session-id";
 
 app.use(
   bodyParser.json({
-    type: function(req) {
+    type: function (req) {
       return /^application\/json/.test(req.get("content-type"));
     },
-  }),
+  })
 );
 
 app.post("/", async (req, res, next) => {
+  const sessionUri = req.get(HEADER_MU_SESSION_ID);
+  console.log("SESSION URI:", sessionUri);
+  if (!sessionUri) {
+    return res.status(401).send("Session ID header not found.");
+  }
+  const groupUriQuery = generateGroupUriSelectQuery(sessionUri);
+  console.log("GROUP URI QUERY:", groupUriQuery);
+  const groupUriResult = await query(groupUriQuery);
+  const groupUri = groupUriResult.results.bindings[0]?.groupUri?.value;
+  if (!groupUri) {
+    return res.status(401).send("User not affiliated with any organization.");
+  }
+  console.log("GROUP URI:", groupUri);
+
   const uploadResourceUuid = req.query.id;
   const selectQuery = generateUploadResourceUriSelectQuery(uploadResourceUuid);
   const result = await query(selectQuery);
@@ -31,12 +47,12 @@ app.post("/", async (req, res, next) => {
   const uploadResourceUri = bindings[0].fileUrl.value;
   const uploadUri = bindings[0].uri.value;
 
-  const filePath = uploadResourceUri.replace("share://", storageFolderPath);
+  const filePath = uploadResourceUri.replace("share://", STORAGE_FOLDER_PATH);
   if (!existsSync(filePath)) {
     return res
       .status(500)
       .send(
-        "Could not find file in path. Check if the physical file is available on the server and if this service has the right mountpoint.",
+        "Could not find file in path. Check if the physical file is available on the server and if this service has the right mountpoint."
       );
   }
   const bpmn = await readFile(filePath, "utf-8");
@@ -45,7 +61,7 @@ app.post("/", async (req, res, next) => {
     triples = await translateToRdf(bpmn, uploadUri);
   } catch (e) {
     console.log(
-      `Something unexpected went wrong while handling bpmn extraction!`,
+      `Something unexpected went wrong while handling bpmn extraction!`
     );
     console.error(e);
     return next(e);
@@ -62,7 +78,7 @@ app.use(errorHandler);
 async function translateToRdf(bpmn, uploadResourceUri) {
   if (!bpmn || bpmn.trim().length === 0) {
     const error = new Error(
-      "Invalid content: The provided file does not contain any content.",
+      "Invalid content: The provided file does not contain any content."
     );
     error.statusCode = 400;
     throw error;
@@ -82,11 +98,11 @@ async function translateToRdf(bpmn, uploadResourceUri) {
   const triples = await RmlMapper.parseTurtle(
     bboMapping(uploadResourceUri),
     inputFiles,
-    options,
+    options
   );
   if (!triples || triples.trim().length === 0) {
     const error = new Error(
-      "Invalid content: The provided file does not contain valid content.",
+      "Invalid content: The provided file does not contain valid content."
     );
     error.statusCode = 400;
     throw error;
--- sparql-queries.js
+++ sparql-queries.js
@@ -1,25 +1,28 @@
-import { sparqlEscapeString } from "mu";
-
-const PREFIXES = `
-   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
-   PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
-   PREFIX dct: <http://purl.org/dc/terms/>
-   PREFIX dbpedia: <http://dbpedia.org/ontology/>
-   PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
-`;
+import { sparqlEscapeString, sparqlEscapeUri } from "mu";
 
 export function generateUpdateQuery(triples) {
   return `INSERT DATA { ${triples} }`;
 }
 
 export function generateUploadResourceUriSelectQuery(uploadResourceUuid) {
-  // prettier-ignore
   return `
-    ${PREFIXES}
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+    PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+    PREFIX dbpedia: <http://dbpedia.org/ontology/>
+    PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
     SELECT * WHERE {
-        ?uri mu:uuid ${sparqlEscapeString(uploadResourceUuid)};
-             nfo:fileName ?fileName ;
-             dbpedia:fileExtension ?extension .
-        ?fileUrl nie:dataSource ?uri .
+      ?uri mu:uuid ${sparqlEscapeString(uploadResourceUuid)};
+           nfo:fileName ?fileName ;
+           dbpedia:fileExtension ?extension .
+      ?fileUrl nie:dataSource ?uri .
     }`;
+}
+
+export function generateGroupUriSelectQuery(sessionUri) {
+  return `
+    PREFIX muExt: <http://mu.semte.ch/vocabularies/ext/>
+    SELECT ?groupUri WHERE {
+      ${sparqlEscapeUri(sessionUri)} muExt:sessionGroup ?groupUri .
+    }
+  `;
 }
