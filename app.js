7ca8fdeef78f1a9354430aa8315be92162efa2fc
Martijn Bogaert <36764916+MartijnBogaert@users.noreply.github.com>
2024-05-31 12:30:26 +0200

    Download bpmn files as images (#4)
    * Add possibility to download bpmn files in different formats
    
    * Fix bug
    
    * Build in null safety
    
    * Install necessary libs to make puppeteer work
    
    * Bring bpmn-to-image code in project
    
    * Don't use esm code
    
    * Use older version of bpmn to image
    
    * Don't unlink temp file
    
    * Add logs
    
    * Add more logs
    
    * Bring feature branch up to date with master (#7)
    
    * Add classification for BPMN elements (#5)
    
    * Add rml rules for bpmn concepts
    
    * Ensure classification objects are IRIs
    
    * Fix typo
    
    * Feature/bpmn-element-classification (#6)
    
    * Add rml rules for bpmn concepts
    
    * Ensure classification objects are IRIs
    
    * Fix typo
    
    * Use dct:type instead of org:classification
    
    * Use non-root user
    
    * Change workdir
    
    * Fix working directory issues
    
    * Make user owner of complete app directory
    
    * Change owner of necessary directories
    
    * Change temporary directory
    
    * Make usage of root user work
    
    * Remove logs
    
    * Remove temporary files

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-import { app, update, query, errorHandler } from "mu";
+import { app, update, query, errorHandler, uuid } from "mu";
 import { querySudo } from "@lblod/mu-auth-sudo";
 import bodyParser from "body-parser";
-
-import { readFile } from "fs/promises";
+import { readFile, unlink } from "fs/promises";
 import * as RmlMapper from "@comake/rmlmapper-js";
 import { mapping as bboMapping } from "./bbo-mapping.js";
 import { existsSync } from "fs";
@@ -12,6 +11,8 @@ import {
   generateGroupUriSelectQuery,
   generateFileGroupLinkInsertQuery,
 } from "./sparql-queries.js";
+import { convert as convertBpmn } from "bpmn-to-image";
+import path from "path";
 
 const STORAGE_FOLDER_PATH = "/share/";
 const HEADER_MU_SESSION_ID = "mu-session-id";
@@ -65,9 +66,6 @@ app.post("/", async (req, res, next) => {
   try {
     bboTriples = await translateToRdf(bpmnFile, virtualFileUri);
   } catch (e) {
-    console.log(
-      `Something unexpected went wrong while handling bpmn extraction!`
-    );
     console.error(e);
     return next(e);
   }
@@ -77,6 +75,48 @@ app.post("/", async (req, res, next) => {
   return res
     .status(200)
     .send({ message: "bpmn extraction completed successfully" });
+});
+
+app.get("/:id/download", async (req, res) => {
+  const acceptType = req.headers["accept"];
+  let tempFilePath = path.join("/tmp", uuid());
+  if (acceptType?.includes("image/svg+xml")) {
+    tempFilePath += ".svg";
+  } else if (acceptType?.includes("image/png")) {
+    tempFilePath += ".png";
+  } else if (acceptType?.includes("application/pdf")) {
+    tempFilePath += ".pdf";
+  } else {
+    return res.status(406).send("The requested file format is not available.");
+  }
+
+  const virtualFileUuid = req.params.id;
+  const fileUriQuery = generateFileUriSelectQuery(virtualFileUuid);
+  const fileUriResult = await query(fileUriQuery);
+  const fileUriBindings = fileUriResult.results.bindings;
+  if (fileUriBindings.length === 0) {
+    return res.status(404).send("Not Found");
+  }
+  const physicalFileUri = fileUriBindings[0].physicalFileUri.value;
+
+  const bpmnFilePath = physicalFileUri.replace("share://", STORAGE_FOLDER_PATH);
+  if (!existsSync(bpmnFilePath)) {
+    return res
+      .status(500)
+      .send(
+        "Could not find file in path. Check if the physical file is available on the server and if this service has the right mountpoint."
+      );
+  }
+
+  try {
+    await convertBpmn(bpmnFilePath, tempFilePath);
+    return res.sendFile(tempFilePath, {}, async () => {
+      await unlink(tempFilePath);
+    });
+  } catch (error) {
+    console.error("error:", error);
+    return res.status(500).send(`Conversion error: ${error.message}`);
+  }
 });
 
 app.use(errorHandler);
